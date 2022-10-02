04506ebd9cb1562f4e61db204d2f616e049c6c31
Christopher-at-694205907733 <cfox1290@gmail.com>
2022-10-02 11:27:21 -0500

    adding in initial global nagivation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,10 @@
 	import IconButton, { Icon } from '@smui/icon-button';
 	import { mdiMenu, mdiWeatherNight, mdiWeatherSunny } from '@mdi/js';
 	import { Svg } from '@smui/common/elements';
+	import Drawer, { AppContent, Content, Header, Title, Subtitle, Scrim } from '@smui/drawer';
+	import Button, { Label } from '@smui/button';
+	import List, { Item, Text, Graphic, Separator, Subheader } from '@smui/list';
+	import { H6 } from '@smui/common/elements';
 
 	let topAppBar: TopAppBarComponentDev;
 
@@ -16,6 +20,14 @@
 	onMount(() => {
 		darkTheme = window.matchMedia('(prefer-color-scheme: dark)').matches;
 	});
+
+	let open = false;
+	let active = 'Menu';
+
+	function setActive(value: string) {
+		active = value;
+		open = false;
+	}
 </script>
 
 <svelte:head>
@@ -31,10 +43,11 @@
 	{/if}
 </svelte:head>
 
+<!-- HEADER -->
 <TopAppBar bind:this={topAppBar} variant="standard">
 	<Row>
 		<Section>
-			<IconButton>
+			<IconButton on:click={() => (open = !open)}>
 				<Icon component={Svg} viewBox="0 0 24 24">
 					<path fill="currentColor" d={mdiMenu} />
 				</Icon>
@@ -51,6 +64,99 @@
 	</Row>
 </TopAppBar>
 
-<AutoAdjust {topAppBar}>
-	<slot />
-</AutoAdjust>
+<!-- DRAWER -->
+<div class="drawer-container">
+	<!-- Don't include fixed={false} if this is a page wide drawer.
+		  It adds a style for absolute positioning. -->
+	<Drawer variant="modal" fixed={false} bind:open>
+		<Header>
+			<a href="/" on:click={() => setActive('')}><Title>Totally Legit Grow Management App</Title></a
+			>
+			<Subtitle>It's pretty legit.</Subtitle>
+		</Header>
+		<Content>
+			<List>
+				<!-- MAIN NAV LIST -->
+				<Item
+					href="/grow-mediums"
+					on:click={() => setActive('GrowMediums')}
+					activated={active === 'GrowMediums'}
+				>
+					<Text>Grow Mediums</Text>
+				</Item>
+				<Item
+					href="/nutrients"
+					on:click={() => setActive('Nutrients')}
+					activated={active === 'Nutrients'}
+				>
+					<Text>Nutrients</Text>
+				</Item>
+				<Item
+					href="/organizations"
+					on:click={() => setActive('Organizations')}
+					activated={active === 'Organizations'}
+				>
+					<Text>Organizations</Text>
+				</Item>
+				<Item
+					href="/plant-categories"
+					on:click={() => setActive('PlantCategories')}
+					activated={active === 'PlantCategories'}
+				>
+					<Text>Plant Categories</Text>
+				</Item>
+				<Item
+					href="/plant-lifecycles"
+					on:click={() => setActive('PlantLifecycles')}
+					activated={active === 'PlantLifecycles'}
+				>
+					<Text>Plant Lifecycles</Text>
+				</Item>
+				<Item
+					href="/plant-stages"
+					on:click={() => setActive('PlantStages')}
+					activated={active === 'PlantStages'}
+				>
+					<Text>Plant Stages</Text>
+				</Item>
+				<Item
+					href="/products"
+					on:click={() => setActive('Products')}
+					activated={active === 'Products'}
+				>
+					<Text>Products</Text>
+				</Item>
+				<Item href="/seeds" on:click={() => setActive('Seeds')} activated={active === 'Seeds'}>
+					<Text>Seeds</Text>
+				</Item>
+
+				<Separator />
+				<!-- MEMBER SPECIFIC NAV LIST -->
+				<Subheader component={H6}>Member Name</Subheader>
+				<Item href="/tasks" on:click={() => setActive('Tasks')} activated={active === 'Tasks'}>
+					<Text>Tasks</Text>
+				</Item>
+				<Item
+					href="/settings"
+					on:click={() => setActive('Settings')}
+					activated={active === 'Settings'}
+				>
+					<Text>Settings</Text>
+				</Item>
+			</List>
+			<div class="flex flex-col m-2">
+				<Button on:click={() => alert('TODO: Logout')} class="mt-32 " color="secondary">
+					<Label>Logout</Label>
+				</Button>
+				<p class="text-xs mt-2">
+					Copyright 2022 Fox Design Agency. All rights reserved. Use of this application is governed
+					by a MIT-style license.
+				</p>
+			</div>
+		</Content>
+	</Drawer>
+	<Scrim fixed={false} />
+	<AutoAdjust {topAppBar}>
+		<slot />
+	</AutoAdjust>
+</div>
